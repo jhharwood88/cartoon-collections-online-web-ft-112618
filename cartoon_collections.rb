d9d020be7c76a2707eeb216323e05339beda12c6
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-29 03:47:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  
+  while i <= array.length
+  array.each_with_index{ |x,y| puts "#{y+1} #{x}"}
+  i += 1
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veg_array)
+  new_array = []
+  veg_array.map { |x| new_array << "#{x.capitalize}!" }
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(element_values)
+  i = 0
+  while i < element_values.length
+    return element_values[i] if yield(element_values[i])
+    i = i + 1
+  end
 end
 
 def find_the_cheese# code an argument here
