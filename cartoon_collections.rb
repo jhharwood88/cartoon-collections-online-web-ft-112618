53e50a1336c067583e456840d0c9e2e344b067e5
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-29 01:11:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  
+  while x <= dwarves.length
+  dwarves.each_with_index{ |x,y| puts "#{x}. #{y}"}
+  i += 1
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
